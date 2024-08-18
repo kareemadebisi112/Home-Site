bafb4a28eecda8506306d8e432750858318bc1a6
Kareem Adebisi <139369860+kareemadebisi112@users.noreply.github.com>
2024-08-18 10:11:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,10 +39,7 @@ unity = fh.Div('Unity', cls='tag tool row')
 salesforce = fh.Div('Salesforce', cls='tag tool row')
 react = fh.Div('React', cls='tag tool row')
 
-about_me = fh.P('''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 
-                standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
-                five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
-                Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.''',cls='about-me')
+about_me = fh.P('''[Coming Soon]''',cls='about-me')
 # Company Text
 rtx_description = fh.P('''Spearheaded comprehensive code reviews, ensuring quality and adherence to best practices.
                         Empowered junior developers through impactful mentorship, fostering their growth and 
